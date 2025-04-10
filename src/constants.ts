25a7b9f908fe555677fa4d33f3a37fb4c89dfb9b
Naveen <naveensen8899@gmail.com>
2025-04-10 14:55:54 +0530

    5

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 const Strings ={
-    ApiUrl:"https://localhost:44310"
-    // dropDownApi:"https://bynaveen.loca.lt"
+    // ApiUrl:"https://localhost:44310"
+    ApiUrl:"https://bynaveen.loca.lt"
 }
 
 export default Strings;
--- src/index.tsx
+++ src/index.tsx
@@ -13,8 +13,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
    {/* <WatherReport /> */}
-   {/* <DropDowns/> */}
-   <DataInsert/>
+   <DropDowns/>
+   {/* <DataInsert/> */}
    {/* <Dashboard/> */}
   </React.StrictMode>
 );
